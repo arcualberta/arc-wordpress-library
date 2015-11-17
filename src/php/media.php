83b457c27e7cf272af804abcf8e6252b5f57e57b
Omar Rodriguez Arenas <orodrigu@ualberta.ca>
2015-11-17 14:32:49 -0700

    Added media layout function

---
+++ src/php/media.php
@@ -0,0 +1,56 @@
+<?php
+namespace Awl;
+defined('ABSPATH') or die('No');
+
+function get_title_desc_media($args) {
+
+	$result = "";
+
+	if (!array_key_exists("data", $args)) {
+		return $result;
+	}
+
+	// container_id
+	// container_class
+	// image_class
+	// title_class
+	// content_class
+
+	if (!array_key_exists('container_id', $args)) {
+		$args['container_id'] = '';
+	}
+	if (!array_key_exists('container_class', $args)) {
+		$args['container_class'] = '';
+	}
+	if (!array_key_exists('image_class', $args)) {
+		$args['image_class'] = '';
+	}
+	if (!array_key_exists('title_class', $args)) {
+		$args['title_class'] = '';
+	}
+	if (!array_key_exists('content_class', $args)) {
+		$args['content_class'] = '';
+	}
+
+
+
+	foreach ($args['data'] as $post) {
+		$result .= "<div id='".$args['container_id']."' class='".$args['container_class']."'>";
+		$result .= 	"<div class='".$args['image_class']."' style='background: url(\"".$post->_arc_image_grid_img."\")'>";
+		$result .= 	"</div>";
+		$result .= 	"<div>";
+		$result .= 		"<div class='".$args['title_class']."'>";
+		$result .= 			$post->post_title;
+		$result .= 		"</div>";
+		$result .= 		"<div class='".$args['content_class']."'>";
+		$result .=  		arc_limit_content("", $post->post_content, 140);
+		$result .= 		"</div>";
+		$result .= 		"<div>";
+		$result .= 			"<a href='".$post->guid."'>More</a>";
+		$result .= 		"</div>";
+		$result .= 	"</div>";
+		$result .= "</div>";
+	}
+
+	return $result;
+}
